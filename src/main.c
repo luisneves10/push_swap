f8709471b44c4078ea0e3cfc5055c1982dee505d
Luís Barbosa <luibarbo@c1r12s7.42porto.com>
2024-06-14 14:46:09 +0100

    fixed memory leak

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: luibarbo <luibarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:20:17 by luibarbo          #+#    #+#             */
-/*   Updated: 2024/06/12 11:50:13 by luibarbo         ###   ########.fr       */
+/*   Updated: 2024/06/14 14:45:25 by luibarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	main(int argc, char **argv)
 {
 	t_stacks	*stacks;
 
+	if (argc < 2)
+		return (0);
 	stacks = malloc(sizeof(t_stacks));
 	if (argc > 2)
 		stacks = params_to_stack(argv + 1, stacks);
 	else if (argc == 2)
 		stacks = str_to_stack(argv[1], stacks);
-	else
-		return (0);
 	if (!stacks)
 		return (print_error());
 	if (!is_sorted(stacks))
